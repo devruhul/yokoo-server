f667e0a25f6ddc38141350b152be022440732bed
devruhul <ruhulamin262001@gmail.com>
2022-08-25 02:48:34 +0600

    feat: send bicycle to database

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6jlv6.mongodb.net/?retryWrites=true&w=majority`;
@@ -23,11 +22,9 @@ async function run() {
         // send bicycle to database
         app.post('/bicycle', async (req, res) => {
             const bicycle = req.body;
-            console.log(bicycle);
             const result = await bicyclesCollection.insertOne(bicycle);
-            res.json(result);
+            res.send(result);
         });
-
 
 
     } finally {
