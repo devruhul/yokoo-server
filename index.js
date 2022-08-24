1816bd1b79be7e99fcc1c68ed5507a7e7f81583f
devruhul <ruhulamin262001@gmail.com>
2022-08-25 01:53:31 +0600

    feat: initialize mongodb and hide db user and pass and create route to post bicycle data , hide .env file

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
-// create express js server
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6jlv6.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const database = client.db("yokooBicycle");
+        const bicyclesCollection = database.collection("bicycles");
+
+        // send bicycle to database
+        app.post('/bicycle', async (req, res) => {
+            const bicycle = req.body;
+            console.log(bicycle);
+            const result = await bicyclesCollection.insertOne(bicycle);
+            res.json(result);
+        });
+
+
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
 
 // create root api
 app.get('/', (req, res) => {
